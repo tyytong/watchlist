cc0c8f8d2545c1ac901b0b8640ed53d219b89afd
yingyingtong <yingyingtong@whale.im>
2021-04-17 17:47:57 +0800

    Add static files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask import url_for
 app = Flask(__name__)
 
-name = 'Grey Li'
+name = 'Test'
 movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
